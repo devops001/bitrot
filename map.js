79c67ab612b32398676c74f6633d8415b06a6974
devops001 <devops001@gmail.com>
2015-01-20 14:42:29 -0800

    Map now tracks entities, has scheduler & engine

--- map.js
+++ map.js
@@ -1,8 +1,11 @@
 
 App.Map = function(tiles) {
-  this.tiles  = tiles;
-  this.width  = tiles.length;
-  this.height = tiles[0].length;
+  this.tiles     = tiles;
+  this.width     = tiles.length;
+  this.height    = tiles[0].length;
+  this.entities  = [];
+  this.scheduler = new ROT.Scheduler.Simple();
+  this.engine    = new ROT.Engine(this.scheduler);
 };
 
 App.Map.prototype.getTile = function(x, y) {
@@ -25,5 +28,15 @@ App.Map.prototype.getRandFloorPos = function() {
     y = Math.floor(Math.random()*this.height);
   } while (this.getTile(x,y) != App.Tiles.floor);
   return {x:x, y:y};
+};
+
+App.Map.prototype.getEntityAt(x, y) {
+  for (var i=0; i<this.entities.length; i++) {
+    var entity = this.entities[i];
+    if (entity.x==x && entity.y==y) {
+      return entity;
+    }
+  }
+  return null;
 };
 
