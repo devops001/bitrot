e85c2c1f138ebe2d4b5019b2dd0d146c0d11b417
devops001 <devops001@gmail.com>
2015-01-26 08:23:49 -0800

    added depth to Map (x,y,z)

--- map.js
+++ map.js
@@ -1,8 +1,9 @@
 
 App.Map = function(tiles, player) {
   this.tiles     = tiles;
-  this.width     = tiles.length;
-  this.height    = tiles[0].length;
+  this.depth     = tiles.length;
+  this.width     = tiles[0].length;
+  this.height    = tiles[0][0].length;
   this.scheduler = new ROT.Scheduler.Simple();
   this.engine    = new ROT.Engine(this.scheduler);
   this.entities  = [];
@@ -21,38 +22,38 @@ App.Map = function(tiles, player) {
 
 // tiles:
 
-App.Map.prototype.getTile = function(x, y) {
-  if (x<0 || x>=this.width || y<0 || y>=this.height) {
+App.Map.prototype.getTile = function(x, y, z) {
+  if (x<0 || x>=this.width || y<0 || y>=this.height || z<0 || z>=this.depth) {
     return App.Tiles.null;
   }
-  return this.tiles[x][y] || App.Tiles.null;
+  return this.tiles[z][x][y] || App.Tiles.null;
 };
 
-App.Map.prototype.getRandOpenPos = function() {
+App.Map.prototype.getRandOpenPos = function(z) {
   var x, y;
   do {
     x = Math.floor(Math.random()*this.width);
     y = Math.floor(Math.random()*this.height);
-  } while (!this.isOpen(x, y));
-  return {x:x, y:y};
+  } while (!this.isOpen(x,y,z));
+  return {x:x, y:y, z:z};
 };
 
-App.Map.prototype.dig = function(x, y) {
-  if (this.getTile(x,y).isDiggable) {
-    this.tiles[x][y] = App.Tiles.floor;
+App.Map.prototype.dig = function(x, y, z) {
+  if (this.getTile(x,y,z).isDiggable) {
+    this.tiles[z][x][y] = App.Tiles.floor;
   }
 };
 
-App.Map.prototype.isOpen = function(x, y) {
-  return this.getTile(x,y).isWalkable && !this.getEntityAt(x,y);
+App.Map.prototype.isOpen = function(x, y, z) {
+  return this.getTile(x,y,z).isWalkable && !this.getEntityAt(x,y,z);
 };
 
 // entities:
 
-App.Map.prototype.getEntityAt = function(x, y) {
+App.Map.prototype.getEntityAt = function(x, y, z) {
   for (var i=0; i<this.entities.length; i++) {
     var entity = this.entities[i];
-    if (entity.x==x && entity.y==y) {
+    if (entity.z==z && entity.x==x && entity.y==y) {
       return entity;
     }
   }
@@ -60,7 +61,8 @@ App.Map.prototype.getEntityAt = function(x, y) {
 };
 
 App.Map.prototype.addEntity = function(entity) {
-  if (entity.x<0 || entity.x>=this.width || entity.y<0 || entity.y>=this.height) {
+  var e = entity;
+  if (e.x<0 || e.x>=this.width || e.y<0 || e.y>=this.height || e.z<0 || e.z>=this.depth) {
     throw new Error('addEntity: entity out of map bounds');
   }
   entity.map = this;
@@ -70,10 +72,11 @@ App.Map.prototype.addEntity = function(entity) {
   }
 };
 
-App.Map.prototype.addEntityAtRandPos = function(entity) {
-  var pos  = this.getRandOpenPos();
+App.Map.prototype.addEntityAtRandPos = function(entity, z) {
+  var pos  = this.getRandOpenPos(z);
   entity.x = pos.x;
   entity.y = pos.y;
+  entity.z = pos.z;
   this.addEntity(entity);
 };
 
@@ -89,7 +92,7 @@ App.Map.prototype.removeEntity = function(entity) {
   }
 };
 
-App.Map.prototype.getEntitiesWithinRadius = function(x, y, radius) {
+App.Map.prototype.getEntitiesWithinRadius = function(x, y, z, radius) {
   var entities = [];
   var left   = x-radius;
   var right  = x+radius;
@@ -97,7 +100,7 @@ App.Map.prototype.getEntitiesWithinRadius = function(x, y, radius) {
   var bottom = y+radius;
   for (var i=0; i<this.entities.length; i++) {
     var e = this.entities[i];
-    if (e.x>=left && e.x<=right && e.y>=top && e.y<=bottom) {
+    if (e.z==z && e.x>=left && e.x<=right && e.y>=top && e.y<=bottom) {
       entities.push(e);
     }
   }
