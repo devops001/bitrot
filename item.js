ac36dd88720c5e7dd277c769731779bc3ec06b46
devops001 <devops001@gmail.com>
2015-01-29 11:33:29 -0800

    added item.describe() & item.describeOne() functions

--- item.js
+++ item.js
@@ -5,6 +5,18 @@ App.Item = function(properties) {
   this.name = properties.name || '';
 };
 
+App.Item.prototype.describe = function() {
+  return this.name;
+};
+
+App.Item.prototype.describeOne = function(shouldCapitalize) {
+  var prefixes = shouldCapitalize ? ['A','An'] : ['a','an'];
+  var desc     = this.describe();
+  var first    = desc.charAt(0).toLowerCase();
+  var prefix   = 'aeiou'.indexOf(first)<0 ? prefixes[0] : prefixes[1];
+  return prefix +" "+ desc;
+};
+
 //------------------------------
 // Repository:
 //------------------------------
