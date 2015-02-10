8ee7d5ae8d464e9b5eb1e16fddb861f36339d63e
devops001 <devops001@gmail.com>
2015-02-10 10:24:39 -0800

    moved describe functions from Item to DynamicGlyph; Item now extends DynamicGlyph

--- app/dynamicGlyph.js
+++ app/dynamicGlyph.js
@@ -30,3 +30,15 @@ App.DynamicGlyph.prototype.hasMixin = function(mixin) {
     return this.mixins[mixin] || this.mixinGroups[mixin];
   }
 };
+
+App.DynamicGlyph.prototype.describe = function() {
+  return this.name;
+};
+
+App.DynamicGlyph.prototype.describeOne = function(shouldCapitalize) {
+  var prefixes = shouldCapitalize ? ['A','An'] : ['a','an'];
+  var desc     = this.describe();
+  var first    = desc.charAt(0).toLowerCase();
+  var prefix   = 'aeiou'.indexOf(first)<0 ? prefixes[0] : prefixes[1];
+  return prefix +" "+ desc;
+};
--- app/item.js
+++ app/item.js
@@ -1,18 +1,7 @@
 
 App.Item = function(properties) {
   properties = properties || {};
-  App.Glyph.call(this, properties);
-  this.name = properties.name || '';
+  App.DynamicGlyph.call(this, properties);
 };
 
-App.Item.prototype.describe = function() {
-  return this.name;
-};
-
-App.Item.prototype.describeOne = function(shouldCapitalize) {
-  var prefixes = shouldCapitalize ? ['A','An'] : ['a','an'];
-  var desc     = this.describe();
-  var first    = desc.charAt(0).toLowerCase();
-  var prefix   = 'aeiou'.indexOf(first)<0 ? prefixes[0] : prefixes[1];
-  return prefix +" "+ desc;
-};
+App.Item.extend(App.DynamicGlyph);
